b784421dc48421588011fd59cfc2c478e42c123c
Maksim Popov <mkspopov@yandex.ru>
2020-01-16 15:17:35 +0300

    Changed template.

--- template.cpp
+++ template.cpp
@@ -1,13 +1,25 @@
-#include <cstdint>
 #include <iostream>
 // ...
 // some other includes
 
-using i32 = int32_t;
+int ReadInput() {
+    int value = 0;
+    std::cin >> value;
+    return value;
+}
 
+void PrintOutput(int answer) {
+    std::cout << answer << '\n';
+}
+
+int FindSquare(int value) {
+    return value * value;
+}
 
 int main() {
-
+    int value = ReadInput();
+    int answer = FindSquare(value);
+    PrintOutput(answer);
 
     return 0;
 }
